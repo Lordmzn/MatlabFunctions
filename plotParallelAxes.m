6cdaed66b3ccca1b4bff92dbf829a3c97da5505d
mxgiuliani <teo.sangria@gmail.com>
2014-02-28 17:19:30 +0100

    upload ParallelAxes function

---
+++ plotParallelAxes.m
@@ -0,0 +1,46 @@
+function fig = plotParallelAxes( X, id )
+
+% fig = plotParallelAxes( X, id )
+%
+% This function draws a parallel-axes plot (Inselberg, 1997), where each
+% element is a line crossing the N-axes at their corresponding values. In
+% the plot, the elements values are normalized between their minimum and
+% maximum values and the axes are oriented from the bottom to the top.
+%
+% Input:    X  = data matrix, where each element is a row and the
+%              N-dimensions are represented on the columns
+%           id = dimension with respect to which the elements are ordered
+%              and colored
+%
+% MatteoG 28/2/2014
+
+% check input consistency
+if(nargin<2)
+    id = 1 ; 
+end
+if(nargin>2)
+    error('too many inputs! please use: plotParallelAxes( X, id ) ');
+end
+[r,c] = size(X);
+if(id>c)
+    error('wrong dimension id! id must be lower than the number of columns of the matrix X');
+end
+
+% normalization
+m = min(X);
+M = max(X);
+Z = ( X - repmat( m,r,1 ) ) ./ ( repmat( M,r,1 ) - repmat( m,r,1 ) ) ;
+
+% alternatives are ordered with respect to the id-th dimension (column) of
+% the matrix
+Y = sortrows( Z,id ) ; 
+c = colormap( jet(r) ) ;
+
+% parallel-axes plot
+figure;
+for i=1:r
+    hold on; parallelcoords( Y( i,: ), 'Color', c( i,: ) ) ; 
+end
+
+fig=1;
+end
